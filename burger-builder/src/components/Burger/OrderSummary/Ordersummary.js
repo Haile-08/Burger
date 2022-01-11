bf4799fbc7153123a846bee7db563c8f3e74734c
Haile Melaku <haile4cmd@gmail.com>
2022-01-11 15:48:22 +0300

    added a button to the modal

--- burger-builder/src/components/Burger/OrderSummary/Ordersummary.js
+++ burger-builder/src/components/Burger/OrderSummary/Ordersummary.js
@@ -21,6 +21,14 @@ const Ordersummary = (props) => {
       </p>
       <ul className=" w-auto font-sans font-mono">{ingredientsummary}</ul>
       <p className="m-3 font-sans font-mono">Continue to cheekout? </p>
+      <div className="flex  justify-center items-center">
+        <button className="text-white m-5 bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+          Continue
+        </button>
+        <button className="text-gray-500 m-5 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600">
+          Cancel
+        </button>
+      </div>
     </Aux>
   );
 };
