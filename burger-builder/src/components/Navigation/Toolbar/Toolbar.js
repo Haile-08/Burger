ca9d7edb74ec847500479e9c58a1ed75c2143bb6
Haile Melaku <haile4cmd@gmail.com>
2022-01-11 16:41:34 +0300

    added the logo

--- burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+import Logo from "../../Logo/Logo";
+
 const Toolbar = (props) => {
   return (
     <header className="h-[56px] w-screen fixed top-0 left-0 bg-orange-700 flex justify-between items-center py-[20px] z-[90] ">
-      <div>Logo</div>
+      <Logo />
       <div>Menu</div>
       <nav className="h-[100%]">nav</nav>
     </header>
